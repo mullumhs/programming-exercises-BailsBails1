2d76d05d5b905001b83148c2ff5b1f795b37a4ba
BailsBails1 <Bailey.cooper17@education.nsw.gov.au>
2023-11-30 11:50:36 +1100

    Update 18-ForLoops.cs

--- 18-ForLoops/18-ForLoops.cs
+++ 18-ForLoops/18-ForLoops.cs
@@ -35,7 +35,7 @@ namespace ProgrammingExercisesIST
             WaitBetween("From 5 to 15:");
 
             // 1. Fix the following FOR loop so that it prints from 5 to 15
-            for (int i = 9999; i < 9999; i++)
+            for (int i = 5; i < 16; i++)
             {
                 Console.WriteLine(i);
             }
@@ -43,24 +43,28 @@ namespace ProgrammingExercisesIST
             WaitBetween("From 20 down to 1:");
 
             // 2. Fix the following loop so that it counts DOWN
-            for (int x = 20; x > 0; x++)
+            for (int i = 20; i >= 1; i--)
             {
-                Console.WriteLine(x);
+
+                Console.WriteLine(i);
             }
 
             WaitBetween("From 90 to 100 by two's:");
 
             // 3. Make the following loop increment by 2 each time
-            for (int count = 90; count <=100; count++)
+            for (int i = 90; i <=100; i+=2)
             {
-                Console.WriteLine(count);
+                Console.WriteLine(i);
             }
 
             WaitBetween("From 60 down to 30 by three's:");
 
             // 4. Make your own FOR loop (press TAB twice quickly) 
             // that counts from 60 down to 30 in steps of 3
-
+            for (int i = 60; i >= 30; i-=3)
+            {
+                Console.WriteLine(i);
+            }
 
 
             WaitBetween("2 to the power of 0 through to 9");
@@ -68,7 +72,10 @@ namespace ProgrammingExercisesIST
             // 5. Make your own FOR loop (press TAB twice quickly).
             // Use it to print out the values of '2 to the power of 0 through to 9'
 
-
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine( Math.Pow(i, 2));
+            }
 
             // Wait at end
             WaitAtEnd();
